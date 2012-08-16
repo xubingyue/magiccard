6dac853e85d7feae2eddaccbbae3c4442c2e5de3
Matt <bobbycalf@gmail.com>
2012-08-16 16:07:58 +0800

    init new editor

---
+++ Editor/Editor/Editor.h
@@ -0,0 +1,32 @@
+
+// Editor.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CEditorApp:
+// See Editor.cpp for the implementation of this class
+//
+
+class CEditorApp : public CWinAppEx
+{
+public:
+	CEditorApp();
+
+// Overrides
+	public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CEditorApp theApp;
